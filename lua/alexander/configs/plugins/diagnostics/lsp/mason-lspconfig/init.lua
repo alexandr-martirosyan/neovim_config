e02ac83cab2c0e8aa44dace04373de65aca0a480
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2025-05-09 23:36:35 -0700

    some changes

--- lua/alexander/configs/plugins/diagnostics/lsp/mason-lspconfig/init.lua
+++ lua/alexander/configs/plugins/diagnostics/lsp/mason-lspconfig/init.lua
@@ -4,6 +4,7 @@ M.opts = {}
 
 M.config = function(_, opts)
 	local lspconfig = require("lspconfig")
+	local mason_lspconfig = require("mason-lspconfig")
 
 	local lsp_settings = require("alexander.configs.plugins.diagnostics.lsp.lspconfig")
 
@@ -13,49 +14,34 @@ M.config = function(_, opts)
 	local handlers = lsp_settings.handlers
 	local disabled_servers = lsp_settings.disabled_servers
 
-	require("mason-lspconfig").setup()
-
-	require("mason-lspconfig").setup_handlers({
-		-- The first entry (without a key) will be the default handler
-		-- and will be called for each installed server that doesn't have
-		-- a dedicated handler.
-		function(server_name) -- default handler (optional)
-			-- disable if lsp was disabled
-			if disabled_servers[server_name] then
-				print(server_name .. " is disabled")
-				return
-			end
-
-			local options = {
-				on_attach = on_attach,
-				on_init = on_init,
-				capabilities = capabilities,
-				handlers = handlers,
-			}
-			server_name = vim.split(server_name, "@")[1]
-
-			local require_ok, conf_opts =
-				pcall(require, "alexander.configs.plugins.diagnostics.lsp.lspconfig." .. server_name)
-			if require_ok then
-				options = vim.tbl_deep_extend("force", conf_opts, options)
-			end
-
-			lspconfig[server_name].setup(options)
-			print(server_name .. " is installed")
-		end,
+	mason_lspconfig.setup({
+		automatic_enable = true,
 	})
 
-	M.setup_circom(lspconfig, on_attach, on_init, capabilities, handlers)
+	local all_servers = mason_lspconfig.get_installed_servers()
+
+	for _, server_name in ipairs(all_servers) do
+		M.setup_server(lspconfig, on_attach, on_init, capabilities, handlers, disabled_servers, server_name)
+	end
+
+	-- setup circom as it is not in repo
+	M.setup_server(lspconfig, on_attach, on_init, capabilities, handlers, disabled_servers, "circom-lsp")
 end
 
-M.setup_circom = function(lspconfig, on_attach, on_init, capabilities, handlers)
+M.setup_server = function(lspconfig, on_attach, on_init, capabilities, handlers, disabled_servers, server_name)
+	-- disable if lsp was disabled
+	if disabled_servers[server_name] then
+		print(server_name .. " is disabled")
+		return
+	end
+
 	local options = {
 		on_attach = on_attach,
 		on_init = on_init,
 		capabilities = capabilities,
 		handlers = handlers,
 	}
-	local server_name = "circom-lsp"
+	-- local server_name = "circom-lsp"
 
 	local require_ok, conf_opts = pcall(require, "alexander.configs.plugins.diagnostics.lsp.lspconfig." .. server_name)
 	if require_ok then
--- lua/alexander/configs/plugins/diagnostics/lsp/mason/init.lua
+++ lua/alexander/configs/plugins/diagnostics/lsp/mason/init.lua
@@ -1,4 +1,4 @@
-local mason_settings = require "alexander.configs.plugins.diagnostics.lsp.mason.settings"
+local mason_settings = require("alexander.configs.plugins.diagnostics.lsp.mason.settings")
 
 local M = {}
 
@@ -12,19 +12,47 @@ M.opts = {
 	},
 
 	max_concurrent_installers = 10,
-	ensure_installed = mason_settings.ensure_installed
+	ensure_installed = mason_settings.ensure_installed,
 }
 
 M.config = function(_, opts)
 	require("mason").setup(opts)
 
+	local already_installed = require("mason-registry").get_installed_package_names()
+
 	vim.api.nvim_create_user_command("MasonInstallAll", function()
+		if opts.ensure_installed and #opts.ensure_installed > 0 then
+			M.InstallAll(opts.ensure_installed, already_installed)
+		end
+	end, {})
+
+	vim.api.nvim_create_user_command("MasonReinstallAll", function()
 		if opts.ensure_installed and #opts.ensure_installed > 0 then
 			vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
 		end
 	end, {})
 
 	vim.g.mason_binaries_list = opts.ensure_installed
+end
+
+M.InstallAll = function(ensure_installed, already_installed)
+	local all_packages_string = ""
+	local already_installed_set = {}
+	for _, v in ipairs(already_installed) do
+		already_installed_set[v] = true
+	end
+
+	for _, pkg_name in ipairs(ensure_installed) do
+		if not already_installed_set[pkg_name] then
+			all_packages_string = all_packages_string .. pkg_name .. " "
+		end
+	end
+
+	local command_to_run = "Mason"
+	if #all_packages_string > 0 then
+		command_to_run = "MasonInstall " .. all_packages_string
+	end
+	vim.cmd(command_to_run)
 end
 
 return M
