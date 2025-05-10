e33ea237bd385698a9225f0be276edf5694606d5
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2025-05-09 21:16:06 -0700

    some changes

--- lua/alexander/configs/autocmds.lua
+++ lua/alexander/configs/autocmds.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
-		local arg = vim.fn.argv(0)
+		local arg = tostring(vim.fn.argv(0))
 		if arg ~= "" then
-			local stat = vim.loop.fs_stat(arg)
+			local stat = vim.uv.fs_stat(arg)
 			if stat and stat.type == "directory" then
 				vim.cmd("cd " .. arg)
 			end
