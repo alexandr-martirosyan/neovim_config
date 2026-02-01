e023d28ce06127d9986c6eb7aa4ce68b34ac29ed
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2026-02-01 06:02:22 -0800

    some changes

--- lua/alexander/plugins/copilot/copilot.lua
+++ lua/alexander/plugins/copilot/copilot.lua
@@ -11,10 +11,10 @@ M.opts = {
 		enabled = false,
 		auto_trigger = false,
 	},
-	nes = {
-		enabled = true, -- requires copilot-lsp as a dependency
-		auto_trigger = false,
-	},
+	-- nes = {
+	-- 	enabled = true, -- requires copilot-lsp as a dependency
+	-- 	auto_trigger = false,
+	-- },
 
 	server_opts_overrides = {
 		offset_encoding = "utf-16", -- Set the offset encoding same as above, see `:h vim.lsp.start` for more info
